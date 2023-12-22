299294bde52b9d6cafa4391956d31d0eacc3ec5f
Saman <samanf74@gmail.com>
2023-12-22 11:06:44 +0000

    make the training more clear

--- fill50k/ControlNet/train_pix2pix.py
+++ fill50k/ControlNet/train_pix2pix.py
@@ -1,46 +1,43 @@
-from dataset.ControlNetDataset import ControlNetDataset
-
+import yaml
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, random_split
-# from cldm.logger import ImageLogger
-from cldm.wandb_logger import WandbImageLogger
+from dataset.ControlNetDataset import ControlNetDataset
 from cldm.model import create_model, load_state_dict
+from cldm.wandb_logger import WandbImageLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
-# from pytorch_lightning.loggers import WandbLogger
 import wandb
-
 from dotenv import dotenv_values
-config = dotenv_values("../../.env")
 
+# Load the configuration file
+with open('train_pix2pix.yaml', 'r') as file:
+    config = yaml.safe_load(file)
 
-DATASET_PATH = f"../../{config['DATASET_PATH_PROCESSED']}"
+DATASET_PATH = config['dataset']['path_processed']
 
-# Configs
-resume_path = f"../../{config['MODEL_CONTROL_NET_PATH']}"
-batch_size = 1
-logger_freq = 1000
-learning_rate = 5 * 1e-5
-sd_locked = True
-only_mid_control = False
-validation_ratio = 0.0002
-seed = 42
-validation_interval = 10000
-
-checkpoint_freq = 1000
-checkpoint_dir =f"../../{config['CHECKPOINT_PATH']}"
-
+# Configs from YAML
+resume_path = config['model']['control_net_path']
+batch_size = config['training']['batch_size']
+logger_freq = config['training']['logger_freq']
+learning_rate = config['model']['learning_rate']
+sd_locked = config['model']['sd_locked']
+only_mid_control = config['model']['only_mid_control']
+validation_ratio = config['dataset']['validation_ratio']
+seed = config['training']['seed']
+validation_interval = config['training']['validation_interval']
+max_epochs = config['training']['max_epochs']
+checkpoint_freq = config['training']['checkpoint_freq']
+checkpoint_dir = config['training']['checkpoint_dir']
 
 pl.seed_everything(seed, workers=True)
 
-
-# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
-model = create_model('./models/scldm_v15.yaml').cpu()
+# Model Creation
+model = create_model(config['model']['config_file']).cpu()
 model.load_state_dict(load_state_dict(resume_path, location='cpu'))
 model.learning_rate = learning_rate
 model.sd_locked = sd_locked
 model.only_mid_control = only_mid_control
 
-# Config the checkpoint
+# Checkpoint configuration
 checkpoint_cb = ModelCheckpoint(
     monitor="train/loss_simple",
     dirpath=checkpoint_dir,
@@ -49,57 +46,48 @@ checkpoint_cb = ModelCheckpoint(
     filename="pix2pix-{epoch:02}-{step:05}",
     mode="min"
 )
-    
 
 print('Start image logger part')
 
-# Create dataset
+# Dataset and Dataloader setup
 dataset = ControlNetDataset(DATASET_PATH, backward=True)
-# Split dataset to train and validation
 train_size = int(len(dataset) * (1 - validation_ratio))
 val_size = len(dataset) - train_size
 train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-# Show the size of train and validation dataset
 print('Train dataset size:', len(train_dataset))
 print('Validation dataset size:', len(val_dataset))
 
-item = dataset[1234]
-jpg = item['jpg']
-txt = item['txt']
-hint = item['hint']
-print(txt)
-print(jpg.shape)
-print(hint.shape)
-
-# Create dataloader
 train_dataloader = DataLoader(train_dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 val_dataloader = DataLoader(val_dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 
-# logger = ImageLogger(batch_frequency=logger_freq)
 logger = WandbImageLogger(batch_frequency=logger_freq, project_name="pix2pix_lite", validation_size=len(val_dataset))
 
 print('End image logger part')
 
-print('Start pytorch Lightening part')
-# import torch
-# torch.set_float32_matmul_precision("medium")
+print('Start PyTorch Lightning part')
 
-# model.cuda()
+# Trainer setup
+trainer = pl.Trainer(
+    devices=1, 
+    callbacks=[logger, checkpoint_cb], 
+    accumulate_grad_batches=4, 
+    accelerator="gpu", 
+    max_epochs=max_epochs, 
+    val_check_interval=validation_interval
+)
+# trainer = pl.Trainer(devices=1, callbacks=[logger, checkpoint_cb], accumulate_grad_batches=4, accelerator="gpu", strategy="deepspeed_stage_2_offload", max_epochs=10000, val_check_interval=validation_interval) #You might also try this strategy but it needs a python script (not interactive environment)
+# trainer.strategy.config["zero_force_ds_cpu_optimizer"] = False
 
-# trainer = pl.Trainer(devices=1, precision="bf16-mixed", callbacks=[logger], accumulate_grad_batches=4, accelerator="gpu")  # But this will be 4x slower
-trainer = pl.Trainer(devices=1, callbacks=[logger, checkpoint_cb], accumulate_grad_batches=4, accelerator="gpu", strategy="deepspeed_stage_2_offload", max_epochs=10000, val_check_interval=validation_interval) #You might also try this strategy but it needs a python script (not interactive environment)
-
-trainer.strategy.config["zero_force_ds_cpu_optimizer"] = False
-
-print('End pytorch Lightening part')
+print('End PyTorch Lightning part')
 
 print('Start fitting part')
 
-# Train!
+# Train
 try:
     trainer.fit(model, train_dataloader, val_dataloader)
 except Exception as e:
+    print(f"Training failed due to {e}")
     wandb.finish()
 
-
+print('End fitting part')
