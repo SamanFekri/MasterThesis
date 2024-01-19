dd6c902d2733f7f1294288ac74062c258707dca5
saman <samanf74@gmail.com>
2024-01-19 14:21:24 +0000

    adding the inference and jupyter labs

--- fill50k/ControlNet/inference.py
+++ fill50k/ControlNet/inference.py
@@ -53,15 +53,29 @@ def run_sampler(
 
         ddim_sampler = DDIMSampler(model)
 
-        img = resize_image(HWC3(input_image), image_resolution)
+#         img = resize_image(HWC3(input_image), image_resolution)
+#         H, W, C = img.shape
+
+#         detected_map = np.zeros_like(img, dtype=np.uint8)
+#         detected_map[np.min(img, axis=2) < 127] = 255
+
+#         control = torch.from_numpy(detected_map.copy()).float().cuda() / 255.0
+#         control = torch.stack([control for _ in range(num_samples)], dim=0)
+#         control = einops.rearrange(control, "b h w c -> b c h w").clone()
+        
+        input_image = HWC3(input_image)
+
+        detected_map = input_image.copy()
+
+        img = resize_image(input_image, image_resolution)
         H, W, C = img.shape
 
-        detected_map = np.zeros_like(img, dtype=np.uint8)
-        detected_map[np.min(img, axis=2) < 127] = 255
+        detected_map = cv2.resize(detected_map, (W, H), interpolation=cv2.INTER_LINEAR)
 
         control = torch.from_numpy(detected_map.copy()).float().cuda() / 255.0
         control = torch.stack([control for _ in range(num_samples)], dim=0)
-        control = einops.rearrange(control, "b h w c -> b c h w").clone()
+        control = einops.rearrange(control, 'b h w c -> b c h w').clone()
+
 
         if seed == -1:
             seed = random.randint(0, 65535)
